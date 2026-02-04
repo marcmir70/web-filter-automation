2f0f1ea4abbf5f61a53eb574e32cab24dc6f19a6
Marc. Mir. <marcmir.std@gmail.com>
2026-02-04 20:54:17 -0300

    v2

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,10 +3,10 @@ from src.scraper import get_python_news
 
 def test_get_python_news_is_list():
     """Checks if the function returns a list (even if it's empty)."""
-    results = get_python_news("Python")
+    results = get_python_news("https://www.python.org/blogs", "Python")
     assert isinstance(results, list)
 
 def test_invalid_keyword_returns_empty():
     """Checks if a non-existent term returns an empty list."""
-    results = get_python_news("InexistenTerm123xYz")
+    results = get_python_news("https://www.python.org/blogs", "InexistenTerm123xYz")
     assert len(results) == 0
